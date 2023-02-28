adf574c9207be872373f93e9eda19662a67a636f
Hezekiah Michael <github.cupel@aleeas.com>
2023-02-28 17:09:39 -0500

    Update the-forward.recipe.py

--- recipes_custom/the-forward.recipe.py
+++ recipes_custom/the-forward.recipe.py
@@ -10,7 +10,7 @@ try:
     from recipes_shared import BasicNewsrackRecipe, WordPressNewsrackRecipe, format_title
 except ImportError:
     # just for Pycharm to pick up for auto-complete
-    from includes.recipes_shared import BasicNewsrackRecipe, WordpressNewsrackRecipe format_title
+    from includes.recipes_shared import BasicNewsrackRecipe, WordpressNewsrackRecipe, format_title
 
 from calibre.web.feeds.news import BasicNewsRecipe
 
