042ca4c1fba9e07878b335c3b0f3f391bb48147d
Hezekiah Michael <spiritomb@protonmail.com>
2023-03-25 20:09:50 -0400

    fiddle with recipe

--- recipes_custom/science-daily.recipe.py
+++ recipes_custom/science-daily.recipe.py
@@ -23,37 +23,36 @@ _name = "Science Daily"
 class ScienceDaily(BasicNewsrackRecipe, BasicNewsRecipe):
     title = _name
     __author__ = u'Darko Miletic'
-    title = _name
     description = ('''ScienceDaily is one of the Internet's most popular science news web sites. Since starting in 1995, the award-winning site has earned the loyalty of students, researchers, healthcare professionals, government agencies, educators and the general public around the world. Now with more than 6 million monthly visitors worldwide, ScienceDaily generates nearly 20 million page views a month and is steadily growing in its global audience.
                              https://www.sciencedaily.com/''')
     conversion_options = {
         'tags' : 'Science, Science Daily, Periodical',
         'authors' : 'newsrack',
     }
-    masthead_url = 'https://www.sciencedaily.com/images/sd-logo.png'
-    oldest_article = 14
+    masthead_url = "https://i.imgur.com/nQ1lgZZ.png"
+    oldest_article = 7
     max_articles_per_feed = 50
     auto_cleanup = True
     use_embedded_content = False
     language = 'en'
     encoding = 'utf-8'
+    delay = 5
 
     # Feed are found here: https://www.sciencedaily.com/newsfeeds.htm
     feeds = [
-
-        ('Health and Medicine', 'https://www.sciencedaily.com/rss/health_medicine.xml'),
-        ('Mind and Brain', 'https://www.sciencedaily.com/rss/mind_brain.xml'),
-        ('Space and Time', 'https://www.sciencedaily.com/rss/space_time.xml'),
-        ('Matter and Energy', 'https://www.sciencedaily.com/rss/matter_energy.xml'),
-        ('Computers and Math', 'https://www.sciencedaily.com/rss/computers_math.xml'),
-        ('Technology News', 'https://www.sciencedaily.com/rss/top/technology.xml'),
-        ('Earth and Climate', 'https://www.sciencedaily.com/rss/earth_climate.xml'),
-        ('Plants and Animals', 'https://www.sciencedaily.com/rss/plants_animals.xml'),
-        ('Fossils and Ruins', 'https://www.sciencedaily.com/rss/fossils_ruins.xml'),
-        ('Society News', 'https://www.sciencedaily.com/rss/science_society.xml'),
-        ('Strange and Offbeat News', 'https://www.sciencedaily.com/rss/strange_offbeat.xml'),
         ('Top Science News', 'https://www.sciencedaily.com/rss/top/science.xml'),
         ('Top News', 'https://www.sciencedaily.com/rss/top.xml'),
+        # ('Health and Medicine', 'https://www.sciencedaily.com/rss/health_medicine.xml'),
+        # ('Mind and Brain', 'https://www.sciencedaily.com/rss/mind_brain.xml'),
+        # ('Space and Time', 'https://www.sciencedaily.com/rss/space_time.xml'),
+        # ('Matter and Energy', 'https://www.sciencedaily.com/rss/matter_energy.xml'),
+        # ('Computers and Math', 'https://www.sciencedaily.com/rss/computers_math.xml'),
+        # ('Technology News', 'https://www.sciencedaily.com/rss/top/technology.xml'),
+        # ('Earth and Climate', 'https://www.sciencedaily.com/rss/earth_climate.xml'),
+        # ('Plants and Animals', 'https://www.sciencedaily.com/rss/plants_animals.xml'),
+        # ('Fossils and Ruins', 'https://www.sciencedaily.com/rss/fossils_ruins.xml'),
+        # ('Society News', 'https://www.sciencedaily.com/rss/science_society.xml'),
+        # ('Strange and Offbeat News', 'https://www.sciencedaily.com/rss/strange_offbeat.xml'),
     ]
 
     def populate_article_metadata(self, article, __, _):
