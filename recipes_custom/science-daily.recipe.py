f276d053f29f0af38c608940520d50450aade1ab
Hezekiah Michael <spiritomb@protonmail.com>
2023-04-04 19:36:20 -0400

    no more autoclean for science daily

--- recipes_custom/science-daily.recipe.py
+++ recipes_custom/science-daily.recipe.py
@@ -34,25 +34,76 @@ class ScienceDaily(BasicNewsrackRecipe, BasicNewsRecipe):
     }
     masthead_url = "https://i.imgur.com/nQ1lgZZ.png"
     oldest_article = 7
+    remove_empty_feeds = True
     max_articles_per_feed = 50
     use_embedded_content = False
     language = 'en'
     encoding = 'utf-8'
-    delay = 5
+    delay = 3
     publication_type = 'newsportal'
-    ignore_duplicate_articles = {}
-    # auto_cleanup_keep = '//*[@id="journal_references"]|//*[@id="story_source"]|//*[@id="date_posted"]|//*[@id="source"]|//*[@id="abstract"]'
-    # remove_tags = ""
     auto_cleanup = False
+    resolve_internal_links = False
+    recursions = 0
+
+    # auto_cleanup_keep = '//*[@id="journal_references"]|//*[@id="story_source"]|//*[@id="date_posted"]|//*[@id="source"]|//*[@id="abstract"]|//p[@id="first"]|//*[@id="story_text"]|//*[@id="text"]|//h1[@id="headline"]'
+    # keep_only_tags = [
+        # dict(id=["journal_references", "story_source", "date_posted", "source", "text", "abstract", "first", "story_text", "headline"]),
+    # ]
+    remove_tags = [
+        classes("logo sharing hr-logo clearfix fullstory"),
+        dict(id='related_releases'),
+        dict(id='related_topics'),
+        dict(id='share_top'),
+        dict(id='topnav'),
+        dict(id='div-gpt-ad-story_bottom'),
+        dict(id='div-gpt-ad-story_middle'),
+        dict(id='div-gpt-ad-story_top'),
+        dict(name='dt', attrs={'class': 'no-print'}),
+        dict(name='dd', attrs={'class': 'no-print'}),
+        dict(name='div', attrs={'class': 'mobile-top-rectangle'}),
+        dict(name='div', attrs={'class': 'mobile-end-rectangle'}),
+        dict(name='ul', attrs={'class': 'topics'}),
+    ]
+    remove_tags_before = [
+        dict(name='h1', attrs={'id': 'headline'}),
+
+    ]
+    remove_tags_after = [
+        dict(name='div', attrs={'id': 'journal_references'}),
+
+    ]
+
+    auto_cleanup = False
+    extra_css = """
+        #abstract,
+        #date_posted,
+        #source,
+        #journal_references{
+            font-size:0.8rem;
+        }
+        h1#headline{
+            font-size:1.75rem;
+        }
+        h2{
+        font-size:1.5rem
+        }
+        p#first{
+            font-size:1.25rem;
+        }
+        #text > p{
+            font-size: 1rem;
+            text-align:left;
+        }
+    """
 
     # Feed are found here: https://www.sciencedaily.com/newsfeeds.htm
     feeds = [
-        #(u'Top Science News', u'https://www.sciencedaily.com/rss/top/science.xml'),
-        (u'Top News', u'https://www.sciencedaily.com/rss/top.xml'),
-        (u'Top Health', u'https://www.sciencedaily.com/rss/top/health.xml'),
-        (u'Top Technology', u'https://www.sciencedaily.com/rss/top/technology.xml'),
-        (u'Top Society', u'https://www.sciencedaily.com/rss/top/society.xml'),
-        (u'Top Environment', u'https://www.sciencedaily.com/rss/top/environment.xml'),
+        (u'Top Science News', u'https://www.sciencedaily.com/rss/top/science.xml'),
+        # (u'Top News', u'https://www.sciencedaily.com/rss/top.xml'),
+        # (u'Top Health', u'https://www.sciencedaily.com/rss/top/health.xml'),
+        # (u'Top Technology', u'https://www.sciencedaily.com/rss/top/technology.xml'),
+        # (u'Top Society', u'https://www.sciencedaily.com/rss/top/society.xml'),
+        # (u'Top Environment', u'https://www.sciencedaily.com/rss/top/environment.xml'),
         # ('Mind and Brain', 'https://www.sciencedaily.com/rss/mind_brain.xml'),
         # ('Space and Time', 'https://www.sciencedaily.com/rss/space_time.xml'),
         # ('Matter and Energy', 'https://www.sciencedaily.com/rss/matter_energy.xml'),
@@ -62,14 +113,12 @@ class ScienceDaily(BasicNewsrackRecipe, BasicNewsRecipe):
         # ('Plants and Animals', 'https://www.sciencedaily.com/rss/plants_animals.xml'),
         # ('Fossils and Ruins', 'https://www.sciencedaily.com/rss/fossils_ruins.xml'),
         # ('Society News', 'https://www.sciencedaily.com/rss/science_society.xml'),
-        (u'Strange and Offbeat News', u'https://www.sciencedaily.com/rss/strange_offbeat.xml'),
+        # (u'Strange and Offbeat News', u'https://www.sciencedaily.com/rss/strange_offbeat.xml'),
     ]
 
     def populate_article_metadata(self, article, __, _):
         if (not self.pub_date) or article.utctime > self.pub_date:
             self.pub_date = article.utctime
-            self.log(article.title)
-            self.log(article.utctime)
             self.title = format_title(_name, article.utctime)
 
     def get_browser(self, *args, **kwargs):
