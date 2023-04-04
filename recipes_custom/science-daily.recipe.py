c6f5ebafbb0d29d16a7bdc85de836650021138f2
Hezekiah Michael <github.cupel@aleeas.com>
2023-04-04 12:18:55 -0400

    Update science-daily.recipe.py [skip ci]

--- recipes_custom/science-daily.recipe.py
+++ recipes_custom/science-daily.recipe.py
@@ -34,12 +34,12 @@ class ScienceDaily(BasicNewsrackRecipe, BasicNewsRecipe):
     }
     masthead_url = "https://i.imgur.com/nQ1lgZZ.png"
     oldest_article = 7
-    # max_articles_per_feed = 50
+    max_articles_per_feed = 50
     use_embedded_content = False
     language = 'en'
     encoding = 'utf-8'
     delay = 5
-    # publication_type = 'newsportal'
+    publication_type = 'newsportal'
     ignore_duplicate_articles = {'url'}
     auto_cleanup_keep = '//*[@id="journal_references"]|//*[@id="story_source"]|//*[@id="date_posted"]|//*[@id="source"]|//*[@id="abstract"]'
     # remove_tags = ""
@@ -47,9 +47,9 @@ class ScienceDaily(BasicNewsrackRecipe, BasicNewsRecipe):
 
     # Feed are found here: https://www.sciencedaily.com/newsfeeds.htm
     feeds = [
-        (u'Top Science News', u'https://www.sciencedaily.com/rss/top/science.xml'),
+        #(u'Top Science News', u'https://www.sciencedaily.com/rss/top/science.xml'),
         (u'Top News', u'https://www.sciencedaily.com/rss/top.xml'),
-        # ('Health and Medicine', 'https://www.sciencedaily.com/rss/health_medicine.xml'),
+        (u'Health and Medicine', u'https://www.sciencedaily.com/rss/health_medicine.xml'),
         # ('Mind and Brain', 'https://www.sciencedaily.com/rss/mind_brain.xml'),
         # ('Space and Time', 'https://www.sciencedaily.com/rss/space_time.xml'),
         # ('Matter and Energy', 'https://www.sciencedaily.com/rss/matter_energy.xml'),
@@ -59,7 +59,7 @@ class ScienceDaily(BasicNewsrackRecipe, BasicNewsRecipe):
         # ('Plants and Animals', 'https://www.sciencedaily.com/rss/plants_animals.xml'),
         # ('Fossils and Ruins', 'https://www.sciencedaily.com/rss/fossils_ruins.xml'),
         # ('Society News', 'https://www.sciencedaily.com/rss/science_society.xml'),
-        # ('Strange and Offbeat News', 'https://www.sciencedaily.com/rss/strange_offbeat.xml'),
+        (u'Strange and Offbeat News', u'https://www.sciencedaily.com/rss/strange_offbeat.xml'),
     ]
 
     def populate_article_metadata(self, article, __, _):
