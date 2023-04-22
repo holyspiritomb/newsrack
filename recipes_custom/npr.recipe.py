63dedbc6fcf323cae5a83765816ff865499fa4f1
Hezekiah Michael <spiritomb@protonmail.com>
2023-04-22 16:24:16 -0400

    fix NPR recipe

--- recipes_custom/npr.recipe.py
+++ recipes_custom/npr.recipe.py
@@ -6,7 +6,6 @@ from calibre.web.feeds.news import BasicNewsRecipe, classes
 from calibre.utils.date import utcnow, parse_date
 from calibre.ebooks.BeautifulSoup import BeautifulSoup
 
-
 # custom include to share code between recipes
 sys.path.append(os.environ["recipes_includes"])
 try:
@@ -35,53 +34,47 @@ class NPR(BasicNewsrackRecipe, BasicNewsRecipe):
     description = (
         '''National Public Radio is an American nonprofit media organization that serves as a national syndicator to a network of over 1,000 public radio stations in the United States.'''
     )
+
     # remove_attributes = ["style", "sizes"]
     # remove_tags_before = dict(name="section", attrs={'id': 'main-section'})
     # remove_tags_after = dict(name='div', attrs={'id': 'storytext'})
-    # remove_tags = [
-    #     dict(name='source', attrs={'data-format': 'webp'}),
-    #     dict(name='source', attrs={'type': 'image/webp'}),
-    #     dict(name='body', attrs={'id': 'blog'}),
-    #     dict(name='input', attrs={'type': 'hidden'}),
-    #     dict(name='div', attrs={'class': 'enlarge_measure'}),
-    #     dict(name='div', attrs={'class': 'enlarge-options'}),
-    #     dict(name='div', attrs={'id': 'primaryaudio'}),
-    #     # dict(name='div', attrs={'aria-label': 'advertisement'}),
-    #     # dict(name='div', attrs={'class': 'ad-config'}),
-    #     dict(name='div', attrs={'class': 'enlarge_html'}),
-    #     dict(name='div', attrs={'class': 'bucketwrap primary'}),
-    #     dict(name='div', attrs={'class': 'bucketwrap'}),
-    #     classes("hide-caption toggle-caption")
-    # ]
+    remove_tags = [
+        dict(name='footer'),
+        dict(name="nav")
+    ]
     # BASE_TEXTONLY = "https://text.npr.org"
     # BASE_RICH = "https://npr.org"
     feeds = [
         (u'National', u'http://www.npr.org/rss/rss.php?id=1003'),
         (u'World', u'http://www.npr.org/rss/rss.php?id=1004'),
-        # (u'National', u'https://feeds.npr.org/1003/rss.xml'),
-        # ('World', 'https://feeds.npr.org/1004/rss.xml'),
+        ('Law', 'https://feeds.npr.org/1070/rss.xml'),
+        ('Politics', 'https://feeds.npr.org/1014/rss.xml'),
+        ('Science', 'https://feeds.npr.org/1007/rss.xml'),
+        ('Space', 'https://feeds.npr.org/1026/rss.xml'),
         ('Climate', 'https://feeds.npr.org/1167/rss.xml'),
         ('Health', 'https://feeds.npr.org/1128/rss.xml'),
-        ('Science', 'https://feeds.npr.org/1007/rss.xml'),
+        # ('Global Health', 'https://feeds.npr.org/1039/rss.xml'),
         ('Race', 'https://feeds.npr.org/1015/rss.xml'),
-        ('Law', 'https://feeds.npr.org/1070/rss.xml'),
-        ('Space', 'https://feeds.npr.org/1026/rss.xml'),
         ('Religion', 'https://feeds.npr.org/1016/rss.xml'),
-        # (u'All', u'http://www.npr.org/rss/rss.php?id=1001'),
-        ('Art and Design', 'https://feeds.npr.org/1047/rss.xml'),
+        ('Books', 'https://feeds.npr.org/1161/rss.xml'),
+        ('Culture', 'https://feeds.npr.org/1008/rss.xml'),
     ]
 
-    # def parse_index(self):
-    #     soup = self.index_to_soup("https://text.npr.org/")
-    #     ans = {}
-    #     articles = {}
-    #     for a in soup.find_all("a", attrs={"class": "topic-title"}):
-    #         article_url = "https://text.npr.org" + a['href']
-    #         article_title = self.tag_to_string(a)
-    #         section = self.tag_to_string(soup.find("p", attrs={"class": "topic-date"}))
-    #         articles = ans.setdefault(section, [])
-    #         articles.append({'title': article_title, 'url': article_url})
-    #     return list(ans.items())
+    extra_css = '''
+        .story-text{
+        font-size: 1rem;
+        }
+        .slug-line, .slug-line a, .slug-line .slug-link{
+        font-size:0.75rem;
+        }
+        h1 {
+        font-size:1.75rem;
+        text-align:left;
+        }
+        h1.story-title ~ p{
+        font-size: 0.75rem;
+        }
+    '''
 
     def populate_article_metadata(self, article, soup, _):
         article_dt_el = soup.find("time")
@@ -89,31 +82,58 @@ class NPR(BasicNewsrackRecipe, BasicNewsRecipe):
             # 2023-04-21T05:10:22-04:00
             article.date = datetime.strptime(article_dt_el['datetime'], "%Y-%m-%dT%H:%M:%S%z")
             article.title = format_title(article.title, article.date)
+        else:
+            article.title = format_title(article.title, article.utctime)
         if (not self.pub_date) or article.utctime > self.pub_date:
             self.pub_date = article.utctime
             self.title = format_title(_name, article.utctime)
 
     def get_article_url(self, article):
-        self.log.warn(article.link)
+        # self.log.warn(article.link)
         article_rich_url = article.link
         article_id = article_rich_url.split('/')[-2]
         article_textonly_url = f"https://text.npr.org/{article_id}"
-        self.log(article_textonly_url)
+        # self.log(article_textonly_url)
         return article_textonly_url
 
+    def preprocess_raw_html(self, raw_html, url):
+        soup = BeautifulSoup(raw_html, from_encoding='utf-8')
+        # self.abort_recipe_processing()
+        para_div = soup.find("div", attrs={"class": "paragraphs-container"})
+        for p in para_div.find("p"):
+            p.class_ = "story-text"
+        header = soup.find("div", attrs={"class": "story-head"})
+        dateline = header.find_all("p")[1]
+        dateline['id'] = "dateline"
+        dateline.name = "span"
+        dateline_ex = dateline.extract()
+        category = soup.find_all("a", attrs={"class": "slug-link"})[1]
+        if category.text == "Sports":
+            self.abort_article(msg="sports")
+        categ_ex = category.extract()
+        header_div = soup.new_tag("div", attrs={"id": "categ-date"})
+        header_div.append(categ_ex)
+        header_div.append(": ")
+        header_div.append(dateline_ex)
+        header.insert_before(header_div)
+        soup.find('p', attrs={"class": "slug-line"}).decompose()
+
+        # datep = firstp.sibling_next("p")
+                # dateline = datetime.strptime(p.text, "%A, %B %-d, %Y • %-I:%M %p %Z")
+                # date_str = datetime.strftime(dateline, "%b %d %Y, %-I:%M %p")
+                # self.log.warn(date_str)
+        return str(soup)
+
     def preprocess_html(self, soup):
-        tier = soup.find("meta", attrs={"property": "article:content_tier"})
-        if tier:
-            if tier['content'] != "free":
-                self.abort_article("Article isn't free.")
-        sect = soup.find("h3", attrs={"class": "slug"})
-        if sect:
-            sect_str = self.tag_to_string(sect.a)
-            if sect_str == "Sports":
-                self.abort_article("Sports article aborted.")
-        # notrans = soup.find("body", attrs={"class": "no-transcript"})
-        # if notrans:
-            # self.abort_article("Aborting article with no transcript.")
+        richlink = soup.find("a", attrs={"class": "full-version-link"})
+        if richlink:
+            richurl = richlink['href']
+            richlink_el = soup.new_tag("a", attrs={"href": richurl})
+            richlink_el.append(f"Full article: {richurl}")
+            full_link_div = soup.new_tag("div", attrs={"id": "rich-link"})
+            full_link_div.append(richlink_el)
+            soup.find("article").append(full_link_div)
+            soup.find("header").decompose()
         return soup
 
 
