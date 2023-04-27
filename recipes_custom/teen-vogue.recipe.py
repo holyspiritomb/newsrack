e81c82a3cad1ac716baf49b887783b7546086c6c
Hezekiah Michael <spiritomb@protonmail.com>
2023-04-26 21:41:16 -0400

    better processing for Teen Vogue [regenerate teen-vogue]

--- recipes_custom/teen-vogue.recipe.py
+++ recipes_custom/teen-vogue.recipe.py
@@ -4,7 +4,7 @@ import sys
 from collections import OrderedDict
 from urllib.parse import urlparse
 from urllib.parse import urljoin
-from datetime import datetime
+from datetime import datetime, timezone
 from calibre import browser
 from calibre.ebooks.BeautifulSoup import BeautifulSoup
 from calibre.web.feeds.news import BasicNewsRecipe, classes
@@ -39,10 +39,12 @@ class TeenVogue(BasicNewsrackRecipe, BasicNewsRecipe):
     ignore_duplicate_articles = {"url"}
     # remove_javascript = True
     resolve_internal_links = False
+    pub_date = ""
     # use_embedded_content = False
     publisher = "Conde Nast"
     masthead_url = "https://www.teenvogue.com/verso/static/teen-vogue/assets/logo.ba28e9df68104824291913727893bf4aaf22e564.svg"
     no_stylesheets = True
+    filter_out = ["horoscope", "zodiac", "retrograde", "astrological", "tampon"]
     keep_only_tags = [
         classes("article__content-header content-header lead-asset article__body"),
         dict(name="div", attrs={"data-testid": "BodyWrapper"}),
@@ -75,6 +77,16 @@ class TeenVogue(BasicNewsrackRecipe, BasicNewsRecipe):
     # feeds = [
     #     ("Teen Vogue", "https://www.teenvogue.com/feed/rss"),
     # ]
+    extra_css = '''
+        h1{font-size:1.75rem;}
+        h2{font-size:1.5rem;}
+        h3{font-size:1.25rem;}
+        .caption__text,.caption__credit{font-size:0.8rem;font-style:italic;}
+        .byline,.article-byline{font-size:0.8rem;}
+        time{text-transform:uppercase;}
+        #article-body-container > p {font-size:1rem;}
+        #article-body-container > div {font-size:0.8rem;}
+    '''
 
     BASE_URL = "https://www.teenvogue.com"
 
@@ -85,68 +97,32 @@ class TeenVogue(BasicNewsrackRecipe, BasicNewsRecipe):
             url_string = urljoin(base_url or self.BASE_URL, url_string)
         return url_string
 
+    def populate_article_metadata(self, article, soup, _):
+        pub_date = soup.find("time")
+        if pub_date:
+            pub_dt = datetime.strptime(pub_date["datetime"], "%Y-%m-%dT%H:%M:%S%z")
+            article.title = format_title(article.title, pub_dt)
+
     def preprocess_raw_html(self, raw_html, url):
-        self.log.warn("preprocess_raw_html: is this is where i figure out how to abort processing of articles that are too old?")
         soup = BeautifulSoup(raw_html)
         pub_date_meta = soup.find(
             name="meta", attrs={"property": "article:modified_time"}
         )
+        self.log.warn(pub_date_meta)
         post_date = datetime.strptime(pub_date_meta["content"], "%Y-%m-%dT%H:%M:%S.%fZ")
         article_age = datetime.utcnow() - post_date
         days_old = article_age.days
-        self.log(f"article at {url} is {days_old} days old")
         if days_old > self.oldest_article:
-            self.log.warn("this article is older than we want")
-            self.abort_article("aborted article")
+            self.abort_article("Aborting article that is too old")
         if not self.pub_date or post_date > self.pub_date:
             self.pub_date = post_date
             self.title = format_title(_name, post_date)
-        # authors = [b.text for b in soup.find_all(attrs={"class": "byline__name-link"})]
-        # category = soup.find("a", attrs={'class': 'rubric__link'}).text
-        # authors_div = soup.new_tag("div", attrs={"class": "author"})
-        # authors_div.append(", ".join(authors))
-        # category_div = soup.new_tag("div", attrs={"class": "category"})
-        # category_div.append(category)
-        # article_original_link = soup.new_tag("a", attrs={"href":url, "id": "sourcelink"})
-        # pub_div = soup.new_tag("div", attrs={"class": "published-dt"})
-        # pub_div.append(f"{post_date:%B %d, %Y %H:%H %p}")
-        # meta_div = soup.new_tag("div", attrs={"class": "article-meta"})
-        # meta_div.append(article_original_link)
-        # meta_div.append(pub_div)
         header = soup.find(
             attrs={"data-testid": "ContentHeaderHed"}
         ) or soup.find("h1")
         header.wrap(soup.new_tag("a", attrs={"data-src": url, "id": "original_link"}))
-        # header.insert_after(meta_div)
-        # soup.find("h1").insert_before(category_div)
-        return str(soup)
-
-    def postprocess_html(self, soup, first_fetch):
-        for a in soup.find_all("a", attrs={"data-src": True}):
-            a["href"] = a["data-src"]
-            del a["data-src"]
-        return soup
-
-    def preprocess_html(self, soup):
-        for a in soup.find_all("a", attrs={"data-event-click": True}):
-            del a["data-event-click"]
-            del a["rel"]
-            del a["target"]
-            del a["data-offer-url"]
-        for img in soup.find_all("img", attrs={"srcset": True}):
-            img["src"] = self._urlize(
-                img["srcset"].strip().split(",")[-1].strip().split(" ")[0]
-            )
-            del img["srcset"]
-        for srcmedia in soup.find_all("source", attrs={"srcset": True, "media": "(max-width: 767px)"}):
-            headerimgsrc = self._urlize(
-                srcmedia["srcset"].strip().split(",")[-1].strip().split(" ")[0]
-            )
-            srcmedia.insert_before(soup.new_tag(name="img", attrs={"src": headerimgsrc, "class": "article-picture"}))
         for srcmed in soup.find_all("source", attrs={"srcset": True}):
             srcmed.decompose()
-        for articlepicture in soup.find_all("img", class_="article-picture"):
-            articlepicture.wrap(soup.new_tag("div"))
         for picture in soup.find_all("picture"):
             # take <img> tag out of <noscript> into <picture>
             noscript = picture.find(name="noscript")
@@ -157,6 +133,39 @@ class TeenVogue(BasicNewsrackRecipe, BasicNewsRecipe):
                 continue
             picture.append(img.extract())
             noscript.decompose()
+        for a in soup.find_all("a", attrs={"href": "https://www.teenvogue.com/newsletter/subscribe"}):
+            a_cont = a.parent
+            if a_cont.name == "p":
+                a_cont.decompose()
+        for a in soup.find_all("a", attrs={"href": "https://www.teenvogue.com/newsletter/subscribe?sourceCode=articlelink"}):
+            a_cont = a.parent
+            if a_cont.name == "p":
+                a_cont.decompose()
+        return str(soup)
+
+    def preprocess_html(self, soup):
+        byline_div = soup.find(attrs={"data-testid": "ContentHeaderAccreditation"})
+        article_subtitle = byline_div.find("div")
+        article_subtitle.name = "h2"
+        for pic in byline_div.find_all("picture"):
+            pic.decompose()
+        datestamp = byline_div.find("time")
+        if datestamp:
+            dt_el = datestamp.extract()
+            categlink = soup.find("a", class_="rubric__link")
+            categlink.insert_after(dt_el)
+            categlink.insert_after(": ")
+        article_container = soup.new_tag("div")
+        article_container["id"] = "article-body-container"
+        for div in soup.find_all("div", class_="article__body"):
+            for el in div.children:
+                article_container.append(el)
+        soup.find("div", attrs={"class": "lead-asset"}).insert_after(article_container)
+        for a in soup.find_all("a", attrs={"data-event-click": True}):
+            del a["data-event-click"]
+            del a["rel"]
+            del a["target"]
+            del a["data-offer-url"]
         for aside in soup.find_all("aside"):
             # tag aside with custom css class
             aside["class"] = aside.get("class", []) + ["custom-aside"]
@@ -164,46 +173,57 @@ class TeenVogue(BasicNewsrackRecipe, BasicNewsRecipe):
             div.unwrap()
         for div in soup.find_all("div", attrs={"data-test": "aspect-ratio-container"}):
             div.unwrap()
+        subhead_byline = soup.find("div", attrs={"data-testid": "ContentHeaderAccreditation"})
+        byline = subhead_byline.find("div", attrs={"data-testid": "BylinesWrapper"})
+        if subhead_byline and byline:
+            byline["class"] = "article-byline"
+            subhead_byline.append(byline.extract())
+        return soup
+
+    def postprocess_html(self, soup, first_fetch):
+        for a in soup.find_all("a", attrs={"data-src": True}):
+            a["href"] = a["data-src"]
+            del a["data-src"]
+        for div in soup.find_all("div", class_="article__body"):
+            div.decompose()
         return soup
 
     def parse_tv_index_page(self, current_url, seen):
-        self.log("running parse_tv_index function on", current_url)
         soup = self.index_to_soup(current_url)
         section = self.tag_to_string(soup.find("h1", attrs={'data-testid': 'SectionHeaderHed'}))
         for a in soup.find("div", attrs={"class": "multi-packages"}).findAll("a", href=True):
             url = a['href']
             if url.startswith("/story"):
-                self.log(url)
                 sect = a.parent.find("a", attrs={'class': 'rubric__link'})
                 subsection = self.tag_to_string(sect)
-                self.log(f"{section} > {subsection}")
                 title = self.tag_to_string(a.find("h3"))
-                if "HOROSCOPE" in title.upper() or "ZODIAC" in title.upper() or "RETROGRADE" in title.upper():
-                    self.log.warn(f"Skipping article: {title}")
-                    continue
-                self.log(title)
+                for word in self.filter_out:
+                    word_allcaps = word.upper()
+                    if word_allcaps in title.upper():
+                        self.log.warn(f"Skipping article that contains '{word}' in title: {title}")
+                        seen.add(url)
+                        break
+                    else:
+                        continue
                 description = None
                 summary = a.parent.find(attrs={"class": "summary-item__dek"})
                 if summary:
                     description = self.tag_to_string(summary)
-                    self.log(description)
                 if title and url not in seen:
                     seen.add(url)
-                    self.log("Found article:", section, title)
                     yield {
                         "title": title,
                         "url": urljoin(self.BASE_URL, url),
                         "description": description,
                         "section": section,
-                        "subsection": subsection,
+                        "subsection": subsection
                     }
 
     def parse_index(self):
-        self.log("running parse_index function")
         sectioned_feeds = OrderedDict()
         articles = []
         seen = set()
-        for pagenum in range(1, 4):
+        for pagenum in range(1, 2):
             articles.extend(
                 self.parse_tv_index_page(
                     f"{self.BASE_URL}/news-politics?page={pagenum}", seen
@@ -244,14 +264,8 @@ class TeenVogue(BasicNewsrackRecipe, BasicNewsRecipe):
             section = article["section"]
             if section not in sectioned_feeds:
                 sectioned_feeds[section] = []
-                self.log("Added section:", section)
             sectioned_feeds[section].append(article)
-        # booksoup = self.index_to_soup("https://www.teenvogue.com/entertainment/books")
-        # healthsoup = self.index_to_soup("https://www.teenvogue.com/wellness/health")
-        # wellness_soup = self.index_to_soup("https://www.teenvogue.com/wellness/mental-health")
-        # voices = self.index_to_soup("https://www.teenvogue.com/wellness/voices")
         return sectioned_feeds.items()
-        # return [(_name, articles)]
 
     def get_browser(self, *args, **kwargs):
         return self
@@ -264,3 +278,6 @@ class TeenVogue(BasicNewsrackRecipe, BasicNewsRecipe):
         return br.open_novisit(*args, **kwargs)
 
     open = open_novisit
+
+
+calibre_most_common_ua = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.87 Safari/537.36'
