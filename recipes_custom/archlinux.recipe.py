f30393dee7f10f17a8195731ea60ecb9b7baa2e2
Hezekiah Michael <spiritomb@protonmail.com>
2023-05-01 22:51:26 -0400

    sort Arch Linux single feed by date

--- recipes_custom/archlinux.recipe.py
+++ recipes_custom/archlinux.recipe.py
@@ -2,12 +2,12 @@
 # vim:fileencoding=utf-8
 import os
 import sys
-# from datetime import timedelta, timezone
+from datetime import timedelta, timezone
 
 # custom include to share code between recipes
 sys.path.append(os.environ["recipes_includes"])
 from recipes_shared import BasicNewsrackRecipe, format_title
-# from calibre.web.feeds import Feed
+from calibre.web.feeds import Feed
 from calibre.web.feeds.news import BasicNewsRecipe
 
 _name = "Arch Linux"
@@ -19,6 +19,7 @@ class ArchLinux(BasicNewsrackRecipe, BasicNewsRecipe):
     max_articles_per_feed = 100
     auto_cleanup = True
     description = "Arch Linux updates and news."
+    use_embedded_content = True
     masthead_url = "https://archlinux.org/static/logos/archlinux-logo-dark-90dpi.ebdee92a15b3.png"
     conversion_options = {
         'tags': 'Arch Linux',
@@ -34,3 +35,53 @@ class ArchLinux(BasicNewsrackRecipe, BasicNewsRecipe):
             self.pub_date = article.utctime
             self.title = format_title(_name, article.utctime)
             # article.title = format_title(article.title, article.utctime)
+
+    def parse_feeds(self):
+        # convert single parsed feed into date-sectioned feed
+        # use this only if there is just 1 feed
+        parsed_feeds = super().parse_feeds()
+        if len(parsed_feeds or []) != 1:
+            return parsed_feeds
+
+        articles = []
+        for feed in parsed_feeds:
+            articles.extend(feed.articles)
+        articles = sorted(articles, key=lambda a: a.utctime, reverse=True)
+        new_feeds = []
+        curr_feed = None
+        parsed_feed = parsed_feeds[0]
+        for i, a in enumerate(articles, start=1):
+            date_published = a.utctime.replace(tzinfo=timezone.utc)
+            date_published_loc = date_published.astimezone(
+                timezone(offset=timedelta(hours=-4))
+            )
+            article_index = f"{date_published_loc:%B %-d, %Y}"
+            if i == 1:
+                curr_feed = Feed(log=parsed_feed.logger)
+                curr_feed.title = article_index
+                curr_feed.description = parsed_feed.description
+                curr_feed.image_url = parsed_feed.image_url
+                curr_feed.image_height = parsed_feed.image_height
+                curr_feed.image_alt = parsed_feed.image_alt
+                curr_feed.oldest_article = parsed_feed.oldest_article
+                curr_feed.articles = []
+                curr_feed.articles.append(a)
+                continue
+            if curr_feed.title == article_index:
+                curr_feed.articles.append(a)
+            else:
+                new_feeds.append(curr_feed)
+                curr_feed = Feed(log=parsed_feed.logger)
+                curr_feed.title = article_index
+                curr_feed.description = parsed_feed.description
+                curr_feed.image_url = parsed_feed.image_url
+                curr_feed.image_height = parsed_feed.image_height
+                curr_feed.image_alt = parsed_feed.image_alt
+                curr_feed.oldest_article = parsed_feed.oldest_article
+                curr_feed.articles = []
+                curr_feed.articles.append(a)
+            if i == len(articles):
+                # last article
+                new_feeds.append(curr_feed)
+
+        return new_feeds
