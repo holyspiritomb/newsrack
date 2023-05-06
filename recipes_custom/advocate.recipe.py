195d0e70cd6a27d340c5591be601a3362f7ff684
Hezekiah Michael <spiritomb@protonmail.com>
2023-05-05 22:39:26 -0400

    mamual cleanup of articles

--- recipes_custom/advocate.recipe.py
+++ recipes_custom/advocate.recipe.py
@@ -6,6 +6,7 @@ import sys
 sys.path.append(os.environ["recipes_includes"])
 from recipes_shared import BasicNewsrackRecipe, format_title
 from calibre.web.feeds.news import BasicNewsRecipe, classes
+from calibre.ebooks.BeautifulSoup import BeautifulSoup
 
 _name = 'The Advocate'
 
@@ -14,7 +15,8 @@ class TheAdvocate(BasicNewsrackRecipe, BasicNewsRecipe):
 
     title = _name
     language = 'en'
-    # use_embedded_content = False
+    use_embedded_content = False
+    recursions = 0
     remove_empty_feeds = True
     masthead_url = "https://upload.wikimedia.org/wikipedia/en/4/46/Advocate1.jpg"
     # masthead_url = "https://www.advocate.com/media-library/eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpbWFnZSI6Imh0dHBzOi8vYXNzZXRzLnJibC5tcy8zMjg0MjU0OC9vcmlnaW4ucG5nIiwiZXhwaXJlc19hdCI6MTcxNDcxMzQ4MH0.rZo95Jiq6Ph8PVSZjmnPedqpyEz0RwIhb9oRpSZVIHg/image.png"
@@ -22,9 +24,34 @@ class TheAdvocate(BasicNewsrackRecipe, BasicNewsRecipe):
     description = "Gay, lesbian, bisexual, transgender, queer news leader including politics, commentary, arts and entertainment - your source for LGBTQ news for over 50 years."
     __author__ = 'holyspiritomb'
     no_stylesheets = True
+    oldest_article = 14
+    # remove_javascript = False
+    remove_attributes = [
+        "style", "height", "width"
+    ]
 
-    # Don't grab articles more than 30 days old
-    oldest_article = 30
+    # keep_only_tags = [
+    #     dict(name="h1", class_="rich-text-headline"),
+    #     dict(name="div", class_="widget__body"),
+    #     dict(name="div", class_="body-description"),
+    # ]
+    remove_tags_after = [
+        dict(name="div", class_="around-the-web"),
+    ]
+    remove_tags_before = [
+        dict(name="h1", class_="widget__headline"),
+    ]
+    remove_tags = [
+        dict(name="div", class_="custom-field-show-as-updated"),
+        # dict(name="div", class_="widget__shares"),
+        dict(name="div", class_="ad-tag"),
+        dict(name="div", class_="around-the-web"),
+    ]
+
+    extra_css = """
+        .social-date-modified::before {content:'Updated: '}
+        .social-date::before {content:'Published: '}
+    """
 
     feeds = [
         ('Families', 'https://www.advocate.com/feeds/families.rss'),
@@ -38,7 +65,26 @@ class TheAdvocate(BasicNewsrackRecipe, BasicNewsRecipe):
         ('Main', 'https://www.advocate.com/feeds/feed.rss'),
     ]
 
+    def preprocess_html(self, soup):
+        tweets = soup.find_all("blockquote", attrs={"class": "twitter-tweet"}) or soup.find_all("blockquote", attrs={"data-twitter-tweet-id": True})
+        if tweets:
+            for tweet in tweets:
+                self.log(tweet)
+                new_tweet = tweet.encode("utf-8").decode("unicode-escape")
+                new_tweet_soup = BeautifulSoup(new_tweet, 'html5lib', from_encoding="unicode")
+                tweet.insert_before(new_tweet_soup)
+                self.log(tweet)
+                self.log(new_tweet_soup)
+                tweet.decompose()
+            # twdiv = tweet.find("div")
+                # if twdiv:
+                    # twtxt = str(twdiv.string)
+                    # twunescape = twtxt.encode("utf-8").decode("unicode-escape")
+                    # twdiv.string = twunescape
+        return soup
+
     def populate_article_metadata(self, article, __, _):
         if (not self.pub_date) or article.utctime > self.pub_date:
             self.pub_date = article.utctime
             self.title = format_title(_name, article.utctime)
+            article.title = format_title(article.title, article.utctime)
