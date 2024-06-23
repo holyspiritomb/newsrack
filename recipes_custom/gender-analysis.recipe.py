ec3418e61396f047902ddfb9e05d1960b51476f4
Hezekiah Michael <spiritomb@protonmail.com>
2024-06-22 23:47:06 -0400

    add source link in headers [skip ci]

--- recipes_custom/gender-analysis.recipe.py
+++ recipes_custom/gender-analysis.recipe.py
@@ -1,13 +1,12 @@
 
 import os
 import sys
+from zoneinfo import ZoneInfo
 from calibre.web.feeds.news import BasicNewsRecipe, classes
-from datetime import timezone, timedelta, datetime, time
 
 sys.path.append(os.environ["recipes_includes"])
 from recipes_shared import BasicNewsrackRecipe, WordPressNewsrackRecipe, format_title
-from calibre.utils.date import utcnow, parse_date
-from calibre.web.feeds import Feed
+from calibre.utils.date import utcnow, parse_date, datetime
 
 _name = "Gender Analysis"
 
@@ -22,7 +21,7 @@ class GenderAnalysis(WordPressNewsrackRecipe, BasicNewsRecipe):
     oldest_article = 60
     max_articles_per_feed = 40
     remove_empty_feeds = True
-    resolve_internal_links = True
+    resolve_internal_links = False
     use_embedded_content = False
 
     feeds = [("Posts", "https://genderanalysis.net/feed/")]
@@ -42,14 +41,22 @@ class GenderAnalysis(WordPressNewsrackRecipe, BasicNewsRecipe):
     ]
 
     extra_css = '''
-    p img{width:98%}
+    p img{max-width:98%}
     #article_source{font-size:0.8rem;}
+    #meta_head{
+        font-size:0.8rem;text-transform:uppercase
+    }
     '''
 
     def populate_article_metadata(self, article, soup, _):
         if (not self.pub_date) or article.utctime > self.pub_date:
             self.pub_date = article.utctime
             self.title = format_title(_name, article.utctime)
+        nyc = ZoneInfo("America/New_York")
+        nyc_dt = datetime.astimezone(article.utctime, nyc)
+        nyc_dt_now = datetime.astimezone(datetime.now(), nyc)
+        datestring = datetime.strftime(nyc_dt, "%b %-d, %Y, %-I:%M %p %Z")
+        datestring_now = datetime.strftime(nyc_dt_now, "%b %-d, %Y, %-I:%M %p %Z")
         source_link_div = soup.new_tag("div")
         source_link_div["id"] = "article_source"
         source_link = soup.new_tag("a")
@@ -57,10 +64,27 @@ class GenderAnalysis(WordPressNewsrackRecipe, BasicNewsRecipe):
         source_link.string = article.url
         source_link_div.append("This article was downloaded from ")
         source_link_div.append(source_link)
+        source_link_div.append(" on ")
+        source_link_div.append(datestring_now)
         source_link_div.append(".")
         hr = soup.new_tag("hr")
         soup.append(hr)
         soup.append(source_link_div)
+
+        head_link = soup.new_tag("a")
+        head_link["href"] = article.url
+        head_link.append("View on Website")
+
+        meta_div = soup.new_tag("div")
+        meta_div["id"] = "meta_head"
+        meta_div.append(article.author)
+        meta_div.append(" | ")
+        meta_div.append(datestring)
+        meta_div.append(" | ")
+        meta_div.append(head_link)
+
+        headline = soup.find("h1")
+        headline.insert_before(meta_div)
 
     def preprocess_html(self, soup):
         imgs = soup.find_all("img")
