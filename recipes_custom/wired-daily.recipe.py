461d71d09973fe59932918ca757fae4458d96113
Hezekiah Michael <spiritomb@protonmail.com>
2024-07-18 14:49:59 -0400

    update wired-daily

--- recipes_custom/wired-daily.recipe.py
+++ recipes_custom/wired-daily.recipe.py
@@ -42,6 +42,7 @@ class WiredDailyNews(BasicNewsrackRecipe, BasicNewsRecipe):
     category = 'news, IT, computers, technology'
     oldest_article = 3
     max_articles_per_feed = 200
+    scale_news_images_to_device = True
     no_stylesheets = True
     encoding = 'utf-8'
     use_embedded_content = False
@@ -124,7 +125,7 @@ class WiredDailyNews(BasicNewsrackRecipe, BasicNewsRecipe):
         for feed in feeds:
             # self.log.debug(feed.title)
             for article in feed.articles[:]:
-                self.log(article.title, "\n", article.url)
+                # self.log(article.title, "\n", article.url)
                 if re.search(regex, article.title):
                     self.log.warn(f"removing {article.title} from feed (regex)")
                     feed.articles.remove(article)
@@ -140,6 +141,12 @@ class WiredDailyNews(BasicNewsrackRecipe, BasicNewsRecipe):
                             continue
                     self.log.debug(f"keeping {article.title} in feed {feed.title}")
         new_feeds = [f for f in feeds if len(f.articles[:]) > 0]
+        self.log("\n\n--------\n\n")
+        for feed in new_feeds:
+            self.log("\n\n--------\n")
+            self.log("Feed: ", feed.title)
+            for article in feed.articles[:]:
+                self.log("\t", article.title, "\n\t", article.url)
         return new_feeds
 
     def populate_article_metadata(self, article, soup, _):
@@ -161,7 +168,8 @@ class WiredDailyNews(BasicNewsrackRecipe, BasicNewsRecipe):
         source_link["href"] = article.url
         source_link.string = article.url
         header_src_a = soup.find("a", attrs={"id": "header_src_a"})
-        header_src_a["href"] = article.url
+        if header_src_a:
+            header_src_a["href"] = article.url
         source_link_div.append("This article was downloaded from ")
         source_link_div.append(source_link)
         source_link_div.append(" on ")
@@ -173,72 +181,77 @@ class WiredDailyNews(BasicNewsrackRecipe, BasicNewsRecipe):
 
     def preprocess_html(self, soup):
         a_soup = soup.find(class_='article main-content')
-        headline = a_soup.find(attrs={'data-testid': "ContentHeaderHed"})
-        subhead = a_soup.find(attrs={'data-testid': 'ContentHeaderAccreditation'})
-        if subhead:
-            subhead["id"] = "subhead"
-            subhead.name = "h2"
-            subhead_text = self.tag_to_string(subhead)
-            subhead.clear()
-            subhead.append(subhead_text)
-        category = a_soup.find("a", class_='rubric__link')
-        category["class"] = 'rubric__link'
+        if a_soup:
+            # self.log(a_soup.prettify())
+            headline = a_soup.find(attrs={'data-testid': "ContentHeaderHed"})
+            subhead = a_soup.find(attrs={'data-testid': 'ContentHeaderAccreditation'})
+            category = a_soup.find("a", class_='rubric__link')
+            category["class"] = 'rubric__link'
+            if subhead:
+                subhead["id"] = "subhead"
+                subhead.name = "h2"
+                subhead_text = self.tag_to_string(subhead)
+                subhead.clear()
+                subhead.append(subhead_text)
 
-        author = a_soup.find("p", attrs={'itemprop': 'author'})
-        author["class"] = 'byline bylines__byline'
+            author = a_soup.find(attrs={'itemprop': 'author'})
+            if author:
+                author["class"] = 'byline bylines__byline'
 
-        lead_pic = a_soup.find("div", class_='lead-asset')
-        if lead_pic:
-            lead_img = lead_pic.find('img')
-            lead_cap = lead_pic.find(attrs={'class': 'caption__credit'})
-            if lead_img and lead_cap:
-                lead_img["id"] = "lead-image"
-                lead_cap["id"] = "lead-image-credit"
+            lead_pic = a_soup.find("div", class_='lead-asset')
+            if lead_pic:
+                lead_img = lead_pic.find('img')
+                lead_cap = lead_pic.find(attrs={'class': 'caption__credit'})
+                if lead_img and lead_cap:
+                    lead_img["id"] = "lead-image"
+                    lead_cap["id"] = "lead-image-credit"
 
-        a_date = a_soup.find("time")
-        a_date["id"] = "article_date"
-        self.log.warn(headline, "\n", a_date)
+            a_date = a_soup.find("time")
+            a_date["id"] = "article_date"
+            self.log.warn(headline, "\n", a_date)
 
-        cat_time = soup.new_tag("div")
-        cat_time["id"] = "categ_date"
-        cat_time.append(category)
-        cat_time.append(" | ")
-        cat_time.append(a_date)
-        cat_time.append(" | ")
-        srclink_span = soup.new_tag("span")
-        srclink_span["id"] = "header_src"
-        srclink_a = soup.new_tag("a")
-        srclink_a["href"] = "#"
-        srclink_a["id"] = "header_src_a"
-        srclink_a.append("View on Wired")
-        srclink_span.append(srclink_a)
-        cat_time.append(srclink_span)
-        new_soup = soup.new_tag("div")
-        new_soup.append(cat_time)
-        new_soup.append(headline)
-        new_soup.append(author)
+            cat_time = soup.new_tag("div")
+            cat_time["id"] = "categ_date"
+            cat_time.append(category)
+            cat_time.append(" | ")
+            cat_time.append(a_date)
+            cat_time.append(" | ")
+            srclink_span = soup.new_tag("span")
+            srclink_span["id"] = "header_src"
+            srclink_a = soup.new_tag("a")
+            srclink_a["href"] = "#"
+            srclink_a["id"] = "header_src_a"
+            srclink_a.append("View on Wired")
+            srclink_span.append(srclink_a)
+            cat_time.append(srclink_span)
 
-        if subhead:
-            new_soup.append(subhead)
-        if lead_pic:
-            if lead_img:
-                new_soup.append(lead_img)
-                if lead_cap:
-                    new_soup.append(lead_cap)
+            new_soup = soup.new_tag("div")
+            new_soup.append(cat_time)
+            new_soup.append(headline)
+            if author:
+                new_soup.append(author)
 
-        content = a_soup.find_all("div", class_='body__inner-container')
-        for piece in content:
-            new_soup.append(piece)
-        new_soup["class"] = 'the_article'
-        for noscript in new_soup.find_all("noscript"):
-            noscript["class"] = "noscript"
-            noscript.name = "div"
-        for div in new_soup.find_all("div", attrs={'aria-level': "3"}):
-            div.name = "h3"
-        a_soup.clear()
-        a_soup.append(new_soup)
+            if subhead:
+                new_soup.append(subhead)
+            if lead_pic:
+                if lead_img:
+                    new_soup.append(lead_img)
+                    if lead_cap:
+                        new_soup.append(lead_cap)
+
+            content = a_soup.find_all("div", class_='body__inner-container')
+            for piece in content:
+                new_soup.append(piece)
+            new_soup["class"] = 'the_article'
+            for noscript in new_soup.find_all("noscript"):
+                noscript["class"] = "noscript"
+                noscript.name = "div"
+            for div in new_soup.find_all("div", attrs={'aria-level': "3"}):
+                div.name = "h3"
+            a_soup.clear()
+            a_soup.append(new_soup)
         return soup
-    
+
     def preprocess_raw_html(self, raw_html, url):
         soup = BeautifulSoup(raw_html)
         head = soup.find("head")
@@ -252,6 +265,26 @@ class WiredDailyNews(BasicNewsrackRecipe, BasicNewsRecipe):
                 else:
                     # self.log.info(f"Didn't find keyword {k} in article {t.string}.")
                     continue
+        lead_pic = soup.find(class_='lead-asset')
+        if lead_pic:
+            src_set = lead_pic.find(attrs={"srcset": True})
+            if src_set:
+                l_srcset = src_set["srcset"].split(", ")
+                image = lead_pic.find("img")
+                if image:
+                    for i in l_srcset:
+                        j = i.split(" ")[1]
+                        k = i.split(" ")[0]
+                        if j == "960w":
+                            image["src"] = k
+                            break
+                        else:
+                            continue
+                    for s in lead_pic.find_all("source"):
+                        s.extract()
+            else:
+                a_soup = soup.find("article")
+                self.log.warn(a_soup)
         return str(soup)
 
     def get_article_url(self, article):
