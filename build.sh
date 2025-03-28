d9d366748d0c34a258a6cb8f0f4e56dfe77c13d8
Hezekiah Michael <spiritomb@protonmail.com>
2025-03-28 17:44:09 -0400

    add some logging

--- build.sh
+++ build.sh
@@ -29,6 +29,8 @@ mkdir -p public meta \
 && cp -p static/theme.compiled.js public/theme.min.js \
 && npx sass -s compressed --no-source-map static/site.scss:static/site.css static/reader.scss:static/reader.css static/viewer-theme-light.scss:public/viewer-theme-light.css static/viewer-theme-dark.scss:public/viewer-theme-dark.css static/opds.scss:public/opds.css \
 && python3 _generate.py "$CI_PAGES_URL" "$GITHUB_SERVER_URL/$GITHUB_REPOSITORY/" "$GITHUB_SHA" "https://github.com/${GITHUB_REPOSITORY}/commit/${GITHUB_SHA}" "${GITHUB_RUN_ID}" "https://github.com/${GITHUB_REPOSITORY}/actions/runs/${GITHUB_RUN_ID}" \
+&& echo "_generate.py done" \
 && node build-index.js < public/lunr_docs.json > public/lunr.json \
+&& echo "build-index.js done" \
 && npx html-minifier-terser --input-dir public/ --output-dir public/ --collapse-whitespace --file-ext html \
 && rm -f *.recipe static/*.compiled.js public/lunr_docs.json
